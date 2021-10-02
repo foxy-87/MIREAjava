4c9d82529ae666ddedd17f7faac94dac6156b404
foxy-87 <40640679+foxy-87@users.noreply.github.com>
2021-10-02 16:29:55 +0300

    Update Main.java

--- pract8/Main.java
+++ pract8/Main.java
@@ -11,16 +11,16 @@ public class Main {
         test1.add("NO");
 
         System.out.println("Вот содержимое в списке test1" + test1);
-        test1.remove(3);
+        test1.remove("NO");
         System.out.println("Вот содержимое в списке test1 после удаления " + test1);
-        test1.moveToBack(0);
+        test1.moveToBack("lol");
         System.out.println("Вот содержимое в списке test1 после переноса " + test1);
 
-        test2.add("lol");
         test2.add("kek");
-        test2.add("cheburek");
-        test2.add("NO");
+        test2.add("lol");
+        test2.add("orbidol");
+        test2.add("NOOOOOOO");
 
-        System.out.println("В списке test2 " + test2.getCapacity() + " и вот содержимое " + test2);
+        System.out.println("В списке test2 " + test2.getCapacity() + " объекта и вот они " + test2);
     }
 }
