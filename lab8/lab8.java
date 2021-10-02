ff9de1cdc3ed5bc250d35a04ce5a4651a30efbf2
foxy-87 <40640679+foxy-87@users.noreply.github.com>
2021-10-02 13:37:23 +0300

    Update lab8.java

--- lab8/lab8.java
+++ lab8/lab8.java
@@ -3,9 +3,9 @@ package mirea.lab8;
 import java.io.*;
 
 public class lab8 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File read = new File("over.txt");
         BufferedReader in = new BufferedReader(new FileReader(read));
-        System.out.println("Сейчас в файле написано:" + in);
+        System.out.println("Сейчас в файле написано:" + in.readLine());
     }
 }
